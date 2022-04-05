735ddcfc3871928362c02f6458d05550158c16d0
maxmay94 <maxmay94@gmail.com>
2022-04-05 17:26:55 -0400

    solve 4, done

--- python-functions.py
+++ python-functions.py
@@ -1,10 +1,10 @@
-# 1. sum_to
-
+# 1.
 def sum_to(arg):
   nums = [num for num in range(1, arg + 1)]
   return sum(nums)
 print(sum_to(6))
 
+# 2.
 def largest(list):
   big = 0
   for num in list:
@@ -13,6 +13,13 @@ def largest(list):
   return big
 print(largest([10, 4, 2, 231, 91, 54]))
 
+# 3.
 def occurrences(str1, str2):
   return str1.count(str2)
 print(occurrences('fleep floop', 'e'))
+
+# 4.
+import math
+def product(*args):
+  return math.prod(args)
+print(product(-1, 4))

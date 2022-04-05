5f7ac7a44fda1d9297009bfaa396ad083af574fb
maxmay94 <maxmay94@gmail.com>
2022-04-05 17:09:33 -0400

    solve 1 & 2

---
+++ python-functions.py
@@ -0,0 +1,15 @@
+# 1. sum_to
+
+def sum_to(arg):
+  nums = [num for num in range(1, arg + 1)]
+  return sum(nums)
+print(sum_to(6))
+
+def largest(list):
+  big = 0
+  for num in list:
+    if num > big:
+      big = num
+  return big
+print(largest([10, 4, 2, 231, 91, 54]))
+
